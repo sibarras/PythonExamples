6702df7052b43d54fbdc541a0f25470aaf22ba57
Samuel Ibarra <ing.samuelibarra@gmail.com>
2020-09-06 14:22:34 -0500

    funcion main en a asterisco

--- heuristic_search/algoritmo_A_asterisco.py
+++ heuristic_search/algoritmo_A_asterisco.py
@@ -1,15 +1,14 @@
 # Algoritmo Heuristico para Encontrar la ruta optima o de menor coste.
 
 class Node:
-    def __init__(self, name:str, value:str, child):
+    def __init__(self, name: str, value: str, child):
         # propiedades de la clase Node
         self.name = name
         self.__char = value
-        
+
         self.childs = []
         self.currentParentLink = 0
-        
-        
+
         # defino los hijos como una lista de nodos
         if type(child) is list:
             self.childs = child
@@ -38,9 +37,10 @@ class Node:
         self.f = sum(self.vals[self.activeParentLink])
         return 'Active Link: ' + str(self.activeParentLink)
 
+
 class AStar:
     def __init__(self, root, goal):
-        
+
         # convierto root y goal a listas
         if type(root) is list:
             self.roots = root
@@ -50,14 +50,12 @@ class AStar:
             self.goals = goal
         else:
             self.goals = [goal]
-    
+
         # Defino variables del Algoritmo
         self.opened = []
         self.closed = []
-        
 
     def runAlgorithm(self):
-        print(diagram+'\n\n')
         self.opened.append(self.roots[0])
         self.currentNode = self.opened[0]
         self.showStep()
@@ -78,7 +76,7 @@ class AStar:
             self.opened.remove(self.currentNode)
             self.closed.append(self.currentNode)
             self.showStep()
-        
+
     def manageChilds(self, childs):
         for child in childs:
             if child in self.opened:
@@ -97,7 +95,7 @@ class AStar:
         while self.currentNode not in self.roots:
             self.totalCost += self.currentNode.f
             self.path.append(self.currentNode)
-            
+
             # nodos que podrian ser el padre
             possibleNode = []
 
@@ -112,50 +110,56 @@ class AStar:
         self.totalCost += self.currentNode.f
         self.path.append(self.currentNode)
         self.path.reverse()
-        print('CAMINO: '+ str(self.path))
-        print('COSTE TOTAL: '+ str(self.totalCost))
+        print('CAMINO: ' + str(self.path))
+        print('COSTE TOTAL: ' + str(self.totalCost))
 
     def showStep(self):
-        print('Actual: '+str(self.currentNode))
-        print('Abiertos: '+str(self.opened))
-        print('Cerrados: '+str(self.closed) + '\n\n')
+        print('Actual: ' + str(self.currentNode))
+        print('Abiertos: ' + str(self.opened))
+        print('Cerrados: ' + str(self.closed) + '\n\n')
 
-diagram = """
-Diagrama del Problema:
 
-    ___ 0+2 ___             ___  A  ___
-  /      |      \\         /      |      \\
- |       |       |       |       |       |
-1+1     1+1     1+3      B       C       D
- |       |       |       |       |       |
-2+1     2+1   - 2+2      E       F     - G
- |       |   |   |       |       |    |  |
-3+1     3+1  |  3+2      H       I    |  J
- |       |   /   |       |       |   /   |
-4+1   4+1->3+1  4+1      K       L -     M 
- |       |       |       |       |       |
-5+0     4+0     5+0      N       O       P
+def main():
+    diagram = """
+    Diagrama del Problema:
 
-"""
+       ___ 0+2 ___             ___  A  ___
+     /      |      \\         /      |      \\
+    |       |       |       |       |       |
+   1+1     1+1     1+3      B       C       D
+    |       |       |       |       |       |
+   2+1     2+1   - 2+2      E       F     - G
+    |       |   |   |       |       |    |  |
+   3+1     3+1  |  3+2      H       I    |  J
+    |       |   /   |       |       |   /   |
+   4+1   4+1->3+1  4+1      K       L -     M 
+    |       |       |       |       |       |
+   5+0     4+0     5+0      N       O       P
 
-# Name = Node ( Name, Value, Child )
-p = Node('P', '5+0', None)
-o = Node('O', '4+0', None)
-n = Node('N', '5+0', None)
-m = Node('M', '4+1', p)
-l = Node('L', '4+1->3+1', o)
-k = Node('K', '4+1', n)
-j = Node('J', '3+2', m)
-i = Node('I', '3+1', l)
-h = Node('H', '3+1', k)
-g = Node('G', '2+2', [j,l])
-f = Node('F', '2+1', i)
-e = Node('E', '2+1', h)
-d = Node('D', '1+3', g)
-c = Node('C' ,'1+1', f)
-b = Node('B' ,'1+1', e)
-a = Node('A', '0+2', [b,c,d]) # Root
+    """
+    print(diagram+'\n\n')
+    # Name = Node ( Name, Value, Child )
+    p = Node('P', '5+0', None)
+    o = Node('O', '4+0', None)
+    n = Node('N', '5+0', None)
+    m = Node('M', '4+1', p)
+    l = Node('L', '4+1->3+1', o)
+    k = Node('K', '4+1', n)
+    j = Node('J', '3+2', m)
+    i = Node('I', '3+1', l)
+    h = Node('H', '3+1', k)
+    g = Node('G', '2+2', [j, l])
+    f = Node('F', '2+1', i)
+    e = Node('E', '2+1', h)
+    d = Node('D', '1+3', g)
+    c = Node('C', '1+1', f)
+    b = Node('B', '1+1', e)
+    a = Node('A', '0+2', [b, c, d])  # Root
 
-algorithm = AStar(a, [n,o,p])
-algorithm.runAlgorithm()
-algorithm.showPath()
+    algorithm = AStar(a, [n, o, p])
+    algorithm.runAlgorithm()
+    algorithm.showPath()
+
+
+if __name__ == '__main__':
+    main()
