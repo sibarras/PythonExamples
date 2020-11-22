1457e1e8d2f83db59758e280335fd113baf74c2f
Samuel Ibarra <ing.samuelibarra@gmail.com>
2020-11-21 23:28:14 -0500

    parte 4

--- Flask/TWT_tutorial/main.py
+++ Flask/TWT_tutorial/main.py
@@ -1,10 +1,24 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, render_template
+from flask import redirect, url_for
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return "<h1>{}</h1>".format(usr)
 
 @app.route("/about")
 def about():
