528aeb4b5a9da926532e1f85269d2def0ebf7b96
Samuel Ibarra <ing.samuelibarra@gmail.com>
2020-11-21 23:50:50 -0500

    parte 5

--- Flask/TWT_tutorial/main.py
+++ Flask/TWT_tutorial/main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from flask import redirect, url_for
-from flask import request
+from flask import request, session
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "Hello"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route("/")
 def home():
@@ -11,14 +14,27 @@ def home():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
+        session.permanent = True
         user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
-    else:
-        return render_template("login.html")
+        session["user"] = user
+        return redirect(url_for("user"))
+    elif "user" in session:
+        return redirect(url_for("user"))
+    
+    return render_template("login.html")
 
-@app.route("/<usr>")
-def user(usr):
-    return "<h1>{}</h1>".format(usr)
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
+
+@app.route('/logout')
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 @app.route("/about")
 def about():
